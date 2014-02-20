7ce865a4ba7315cf8be6897e6fb21167253b2f9a
Alin <alin.ciobanu@cti.pub.ro>
2014-02-20 23:01:06 +0200

    config files

---
+++ src/main/java/ro/bets/config/InfrastructureConfig.java
@@ -0,0 +1,77 @@
+package ro.bets.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * Created by yozmo on 2/20/14.
+ */
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "ro.bets.repository")
+@ComponentScan(basePackages = {"ro.bets.controller", "ro.bets.service"})
+public class InfrastructureConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306");
+        dataSource.setUsername("root");
+        dataSource.setPassword("jovial");
+        return dataSource;
+    }
+
+    protected
+    @Bean
+    JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter ret = new HibernateJpaVendorAdapter();
+        return completeVendor(ret);
+    }
+
+    private AbstractJpaVendorAdapter completeVendor(AbstractJpaVendorAdapter jpaVendorAdapter) {
+        jpaVendorAdapter.setDatabase(Database.MYSQL);
+        jpaVendorAdapter.setGenerateDdl(true);
+        jpaVendorAdapter.setShowSql(true);
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    @Autowired
+    AbstractEntityManagerFactoryBean entityManagerFactory
+            (JpaVendorAdapter jpaVendorAdapter, DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean containerEntityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+        containerEntityManagerFactoryBean.setDataSource(dataSource);
+        containerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        containerEntityManagerFactoryBean.setPackagesToScan(domainPackages());
+        return containerEntityManagerFactoryBean;
+    }
+
+    private   String[] domainPackages(){
+        return new String[]{"ro.teamnet.hero.domain"};
+    }
+
+    @Bean
+    @Autowired
+    public org.springframework.transaction.PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+}
---
+++ src/main/java/ro/bets/config/WebMvcConfig.java
@@ -0,0 +1,54 @@
+package ro.bets.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+/**
+ * Spring MVC Configuration.
+ * @author Craig Walls
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+        "ro.bets.controller"})
+
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/");
+    }
+
+/*    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/home");
+    }*/
+
+/*    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/WEB-INF/resources/messages/messages");
+        return messageSource;
+    }*/
+
+    @Bean
+    public UrlBasedViewResolver urlBasedViewResolver()
+    {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass( JstlView.class );
+        viewResolver.setPrefix( "/WEB-INF/views/" );
+        viewResolver.setSuffix( ".html" );
+        return viewResolver;
+    }
+
+
+}
